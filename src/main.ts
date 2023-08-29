88a38bd7c2eba52f351fbda62f4dfcd29b03c02c
jonwalz <jon.walz.music@gmail.com>
2023-08-29 12:58:39 -0700

    first

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
 import "./style.css";
 
 console.log("test");
+// build form to take a text input
+
+const form = document.querySelector<HTMLFormElement>("#todo-form");
+
+const list = document.querySelector("#todo-list");
+
+form?.addEventListener("submit", function (event) {
+  const form = event.currentTarget as HTMLFormElement;
+  const inputValue = form.elements[0].value;
+
+  event.preventDefault();
+
+  const todo = document.createElement("li");
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function () {
+    todo.remove();
+  });
+
+  todo.innerText = inputValue;
+  todo.appendChild(deleteButton);
+
+  list?.appendChild(todo);
+
+  form.reset();
+});
