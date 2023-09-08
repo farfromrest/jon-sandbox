287e788f5e09fe7ce4cb5bbc2cee33dba8f81637
jonwalz <jon.walz.music@gmail.com>
2023-09-08 12:35:47 -0700

    round 2

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,49 @@
 import "./style.css";
 
 // Render initial todos
-// const todos = ["Buy milk", "Buy eggs", "Buy bread", "Buy rice"];
+const todos = ["Buy milk", "Buy eggs", "Buy bread", "Buy rice"];
 
 const form = document.querySelector<HTMLFormElement>("#todo-form");
 
 const list = document.querySelector("#todo-list");
 
+function render() {
+  // @ts-ignore
+  list.innerHTML = "";
+  todos.forEach((todo, i) => renderTodo(todo, i));
+}
+
+function createTodo(todo: string) {
+  todos.push(todo);
+  render();
+}
+
+function renderTodo(todo: string, i: number) {
+  const item = document.createElement("li");
+  item.innerText = todo;
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.addEventListener("click", function () {
+    todos.splice(i, 1);
+    render();
+  });
+
+  item.appendChild(deleteButton);
+
+  list?.appendChild(item);
+}
+
 form?.addEventListener("submit", function (event) {
   const form = event.currentTarget as HTMLFormElement;
+  // @ts-ignore
   const inputValue = form.elements[0].value;
 
   event.preventDefault();
 
-  const todo = document.createElement("li");
-  const deleteButton = document.createElement("button");
-  deleteButton.innerText = "Delete";
-  deleteButton.addEventListener("click", function () {
-    todo.remove();
-  });
-
-  todo.innerText = inputValue;
-  todo.appendChild(deleteButton);
-
-  list?.appendChild(todo);
+  createTodo(inputValue);
 
   form.reset();
 });
+
+render();
